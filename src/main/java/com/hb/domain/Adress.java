1d60fe3dfbbb7bb6e44ca3a51bfe692da675b7ce
fersatir <fatihersatir@gmail.com>
2022-05-31 12:04:19 +0300

    fatih v2

--- src/main/java/com/hb/domain/Adress.java
+++ src/main/java/com/hb/domain/Adress.java
@@ -1,5 +1,84 @@
 package com.hb.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import com.hb.enums.CityEnums;
+
+@Entity
 public class Adress {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable=false)
+	private String adress;
+	
+	@Enumerated(EnumType.STRING)
+	private CityEnums city;
+	
+	@OneToOne
+	@JoinColumn(name="student_id")
+	private Student student;
+	
+	@OneToOne
+	@JoinColumn(name="teacher_id")
+	private Teacher teacher;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public CityEnums getCity() {
+		return city;
+	}
+
+	public void setCity(CityEnums city) {
+		this.city = city;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	@Override
+	public String toString() {
+		return "Adress [id=" + id + ", adress=" + adress + ", city=" + city + ", student=" + student + ", teacher="
+				+ teacher + "]";
+	}
+	
+	
 
 }
